41fccc340dcfce2cc9211f13793100b89dda0c81
Muzamil Khan <113015136+Muzamilofficial@users.noreply.github.com>
2024-12-27 21:00:03 +0500

    update vercel settings
    update vercel settings

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = 'django-insecure-g_n2+2bznu6e@1wel!i(&-4tp86_7lop5395ww+i4x%9*7^old
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -126,7 +126,7 @@ LOGOUT_URL = 'logout'
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR/'staticFiles_build' / 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
